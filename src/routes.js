59a03008ee31b77e6ba896a7c04866172c3321bb
jwlehman93 <jeremylehman9@gmail.com>
2016-09-28 13:38:32 -0400

    multiple routes working

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import App from './App';
+import Home from './components/Home'
 
 const routes = [
   {path: '/', action: () => <App />},
-  //{path: '/tutorials', action: () => <Tutorials />}
+  {path: '/tutorials', action: () => <Home />}
 ];
 
 export default routes;
