1635b48162c11554da4d7c06ac4b85a17e3fed19
David Tschida <dmtschida1@gmail.com>
2016-11-02 18:41:50 +0000

    Fixed issues that made the site not even load

--- src/components/Home.js
+++ src/components/Home.js
@@ -23,16 +23,22 @@ const Home = (props) => {
             <p>David Tschida, President <a href='mailto:dt+sigapp@purdue.edu'>email</a></p>
             <p>Jeff Hughes, Vice President</p>
             <p>Michael Vieck, Vice President</p>
+            <div className="embed-responsive embed-responsive-4by3">
+                <iframe className="embed-responsive-item" src="https://calendar.google.com/calendar/embed?src=3saj71dvb58gl7hh2bl3g10d08%40group.calendar.google.com&ctz=America/New_York" style={{border: 0}} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
+            </div>
           </div>
-          <iframe src="https://calendar.google.com/calendar/embed?src=3saj71dvb58gl7hh2bl3g10d08%40group.calendar.google.com&ctz=America/New_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
           <div className='col-sm-2 sidenav'>
-            <h4>Purdue Laundry App</h4>
-            <p>Tired of having to go to the laundry room, only to find out that
-            there's no space available? The Purdue Laundry app solves that by
-            having all the details of your dorm's laundry room in the palm of
-            your hand! You can even set timers for each machine you are using
-            to be alerted when they are done.</p>
-            <a href='https://play.google.com/store/apps/details?id=xyz.jhughes.laundry&hl=en&utm_source=global_co&utm_medium=prtnr&utm_content=Mar2515&utm_campaign=PartBadge&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/></a>
+            <div className="row">
+                <h4>Purdue Laundry App</h4>
+                <p>Tired of having to go to the laundry room, only to find out that
+                there's no space available? The Purdue Laundry app solves that by
+                having all the details of your dorm's laundry room in the palm of
+                your hand! You can even set timers for each machine you are using
+                to be alerted when they are done.</p>
+                <a href='https://play.google.com/store/apps/details?id=xyz.jhughes.laundry&hl=en&utm_source=global_co&utm_medium=prtnr&utm_content=Mar2515&utm_campaign=PartBadge&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+                    <img alt='Get it on Google Play' className="img-responsive" src='https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png'/>
+                </a>
+            </div>
           </div>
         </div>
       </div>
